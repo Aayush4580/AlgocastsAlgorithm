4041a2f5e9db0338faab5d3ebdd16b203da8a592
baayush <baayush@deloitte.com>
2020-10-10 20:50:28 +0530

    push

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  var obj = {};
+  for (let char of str) {
+    if (obj[char]) {
+      obj[char]++;
+    } else {
+      obj[char] = 1;
+    }
+  }
+  var res = 0;
+  for (let i in obj) {
+    if (obj[i] > res) {
+      res = obj[i];
+    }
+  }
+  function findValue(obje, val) {
+    for (key in obje) {
+      if (obje[key] === val) {
+        return key;
+      }
+    }
+  }
+  return findValue(obj, res);
+}
 
 module.exports = maxChar;
