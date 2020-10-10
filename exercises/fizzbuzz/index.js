1e263946d63695dda5f2d9d847c2068e021b0333
baayush <baayush@deloitte.com>
2020-10-10 21:31:55 +0530

    push

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -15,19 +15,15 @@
 function fizzBuzz(n) {
   if (n > 0) {
     for (var i = 1; i <= n; i++) {
-      if (n > 0) {
-        for (var i = 1; i <= n; i++) {
-          if (i % 15 === 0) {
-            console.log('fizzbuzz');
-          } else if (i % 3 === 0) {
-            console.log('fizz');
-          } else if (i % 5 === 0) {
-            console.log('buzz');
-          }
-          if (i % 3 != 0 && i % 5 != 0 && i % 15 != 0) {
-            console.log(i);
-          }
-        }
+      if (i % 15 === 0) {
+        console.log('fizzbuzz');
+      } else if (i % 3 === 0) {
+        console.log('fizz');
+      } else if (i % 5 === 0) {
+        console.log('buzz');
+      }
+      if (i % 3 != 0 && i % 5 != 0 && i % 15 != 0) {
+        console.log(i);
       }
     }
   }
