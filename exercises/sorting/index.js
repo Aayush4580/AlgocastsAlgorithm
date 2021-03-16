fed6e7069bd53fded62994d98cd8a96c751fd12b
baayush <baayush@deloitte.com>
2021-03-16 16:12:02 +0530

    push

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,67 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
+  let n = inputArr.length;
 
+  for (let i = 0; i < n; i++) {
+    // Finding the smallest number in the subarray
+    let min = i;
+    for (let j = i + 1; j < n; j++) {
+      if (inputArr[j] < inputArr[min]) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      // Swapping the elements
+      let tmp = inputArr[i];
+      inputArr[i] = inputArr[min];
+      inputArr[min] = tmp;
+    }
+  }
+  return inputArr;
 }
 
-function mergeSort(arr) {
+function mergeSort(array) {
+  const half = array.length / 2;
 
+  // Base case or terminating case
+  if (array.length < 2) {
+    return array;
+  }
+
+  const left = array.splice(0, half);
+  return merge(mergeSort(left), mergeSort(array));
 }
 
 function merge(left, right) {
+  let arr = [];
+  // Break out of loop if any one of the array gets empty
+  while (left.length && right.length) {
+    // Pick the smaller among the smallest element of left and right sub arrays
+    if (left[0] < right[0]) {
+      arr.push(left.shift());
+    } else {
+      arr.push(right.shift());
+    }
+  }
 
+  // Concatenating the leftover elements
+  // (in case we didn't go through the entire left or right array)
+  return [...arr, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
